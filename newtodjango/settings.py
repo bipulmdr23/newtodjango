52ff9fd9112fba17884f43ca9b9a2f4ac527015c
bipul <bipulmdr23@gmail.com>
2020-11-20 23:17:13 +0545

    done

--- newtodjango/settings.py
+++ newtodjango/settings.py
@@ -76,14 +76,18 @@ WSGI_APPLICATION = 'newtodjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Travello',
-        'USER': 'postgres',
-        'PASSWORD': 'bipul',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3')
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'travellodatabase'
+    }
+}
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
